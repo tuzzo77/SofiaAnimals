3490b480a3dde71236102df95fa1e98ef52a3960
Alessandro Mattiuzzi MacPro <AlexMacPro@MBPdiAlessandro.homenet.telecomitalia.it>
2014-09-18 11:39:09 +0200

    Initial Commit

---
+++ SofiaAnimals/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SofiaAnimals
+//
+//  Created by Alessandro Mattiuzzi MacPro on 18/09/14.
+//  Copyright (c) 2014 Alessandro Mattiuzzi MacPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SofiaAnimals/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SofiaAnimals
+//
+//  Created by Alessandro Mattiuzzi MacPro on 18/09/14.
+//  Copyright (c) 2014 Alessandro Mattiuzzi MacPro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
